3502b6738ea26c7935f657b2c7c9e3dca46ebc6d
bloodlotus <jamland101@gmail.com>
2023-12-02 17:02:19 -0500

    main

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -4,47 +4,47 @@ class Sudoku:
     def __init__(self, row_length, removed_cells):
         self.row_length = row_length
         self.removed_cells = removed_cells
-	    self.board = [[0] * row_length for _ in range(row_length)]
-	    self.box_length = int(sqrt(row_length))
+        self.board = [[0] * row_length for _ in range(row_length)]
+        self.box_length = int(sqrt(row_length))
 
     def get_board(self):
         return self.board
-    # Returns 2D board
+        # Returns 2D board
     def print_board(self):
         print(get_board(self))
-    # Prints board
+        # Prints board
     def valid_in_row(self, row, num):
         for i in self.board[row]:
             if num == i:
                 return False
         return True
-    # checks if num is in row of board
+        # checks if num is in row of board
     def valid_in_col(self, col, num):
-        for i in range(0, self.board)
-            for j in self.board[i][col]
+        for i in range(0, self.board):
+            for j in self.board[i][col]:
                 if num == j:
-                return False
-        return True
-    # checks if num is in col of board
-    def valid_in_box(self, row_start, col_start, num):
-        for i in range(row_start, row_start+2):
-            for j in range(col_start, col_start+2)
-                if self.board[i][j] == num
                     return False
         return True
-    # checks if num is in box
+        # checks if num is in col of board
+    def valid_in_box(self, row_start, col_start, num):
+        for i in range(row_start, row_start+2):
+            for j in range(col_start, col_start+2):
+                if self.board[i][j] == num:
+                    return False
+        return True
+        # checks if num is in box
     def is_valid(self, row, col, num):
         if self.board[row][col] != real_board[row][col]:
             return False
         else:
             return True
-    # checks if input value is correct
+        # checks if input value is correct
     def fill_box(self, row_start, col_start):
         num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         for i in range(3):
             for j in range(3):
                 self.board[row_start + i][i + col_start] = num.pop(randomrandrange(len(num)))
-    # fills box.
+        # fills box.
 
     def fill_diagonal(self):
         for i in range(3):
@@ -92,7 +92,7 @@ class Sudoku:
             else:
                 self.removed_cells += 1
         return self.board
-    # Removes cells based on difficulty
+        # Removes cells based on difficulty
 def generate_sudoku(size, removed):
     sudoku = SudokuGenerator(size, removed)
     sudoku.fill_values()
